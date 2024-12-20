2544fd6a597df2fe627c20d2e6dbfcfaa12e641f
Joshua Rose <joshuarose@gmx.com>
2024-12-20 12:17:24 +1100

    feat: removed gruvbox colorscheme
     * made alt the primary mod key
     * removed lang from dwmblocks
     * changed font to fira code
     * increased status bar font size

--- startdwm.sh
+++ startdwm.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-wallpaper="~/Pictures/Wallpapers/13-Autumn-trees-Foliage.jpg"
-
 [ -f "~/.Xresources" ] && xrdb -merge "~/.Xresources"
 [ -f "~/.Xmodmap" ] && xmodmap "~/.Xmodmap"
 
-[ -f "$wallpaper" ] && feh --bg-fill --no-fehbg --no-xinerama "$wallpaper" &
-[ -z "$DISPLAY" ] && loadkeys colemak || setxkbmap us -variant colemak -option 'ctrl:nocaps'
-
 [ pidof xcompmgr ]] >/dev/null && killall xcompmgr
 xcompmgr -n &
+
+setxkbmap us -variant colemak -option 'ctrl:nocaps'
+
+kitty &
+feh --bg-fill "/home/t_doctor/Pictures/Wallpapers/13-Autumn-trees-Foliage.jpg" &
 
 dwmblocks &
 
